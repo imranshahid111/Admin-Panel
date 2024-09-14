8392c020cf05e07292cef2a7e53ee97161799688
Imran Shahid <imranshahid111@gmail.com>
2024-09-15 01:16:26 +0500

    fecth

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -6,7 +6,7 @@ import ExpressFormidable from "express-formidable";
 
 const router = express.Router();
 
-router.post ('/create-post' , requireSignIn , ExpressFormidable() , isAdmin , createPostController);
+router.post ('/create-post' ,  ExpressFormidable()  , createPostController);
 router.get('/get-all-product' , getAllProducts);
 router.get('/get-product/:slug' , getProduct);
 router.get('/get-photo/:pid',getPhotoOfProduct);
